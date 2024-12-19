eed508204d80500db570ba4c16668f4815138690
Senadin Puce <senadin.puce@gmail.com>
2024-12-19 15:38:40 +0100

    add chat history

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ st.header("LLM Chatbot")
 
 prompt = st.text_input("Prompt", placeholder="Enter your promt here...")
 
-if ("user_prompt_history" not in st.session_state 
+if (
+    "user_prompt_history" not in st.session_state
     and "chat_answers_history" not in st.session_state
+    and "chat_history" not in st.session_state
 ):
     st.session_state["user_prompt_history"] = []
     st.session_state["chat_answers_history"] = []
+    st.session_state["chat_history"] = []
 
 if prompt:
     with st.spinner("Generating response..."):
-        generated_response = retrieval_qa(query=prompt)
+        generated_response = retrieval_qa(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
 
-    answer = generated_response['answer']
+        answer = generated_response["answer"]
 
-    st.session_state["user_prompt_history"].append(prompt)
-    st.session_state["chat_answers_history"].append(answer)
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(answer)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", answer))
 
 if st.session_state["chat_answers_history"]:
-    for answer, user_query in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
+    for answer, user_query in zip(
+        st.session_state["chat_answers_history"],
+        st.session_state["user_prompt_history"],
+    ):
         message(answer)
         message(user_query, is_user=True)
--- qa/retrieval_qa.py
+++ qa/retrieval_qa.py
@@ -1,36 +1,36 @@
 import os
-
 from dotenv import load_dotenv
+from typing import Any, Dict, List
+
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-from openai import embeddings
 
 load_dotenv()
 
-
-def retrieval_qa(query: str):
+def retrieval_qa(query, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     vector_store = PineconeVectorStore(
         index_name=os.environ["PINECONE_INDEX_NAME"], embedding=embeddings
     )
     chat = ChatOpenAI(model="gpt-4o-mini", verbose=True, temperature=0)
 
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=vector_store.as_retriever(), combine_docs_chain=stuff_documents_chain
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=vector_store.as_retriever(), prompt=rephrase_prompt
     )
 
-    result = qa.invoke(input={"input": query})
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
 
     return result
-
-
-if __name__ == "__main__":
-    query = "What is the difference between a monolith and microservices?"
-    result = retrieval_qa(query)
-    print(result)
